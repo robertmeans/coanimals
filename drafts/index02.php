16f9471c10decf647df7222783b3250495ab3f04
robertmeans <robert@robertmeans.com>
2017-09-29 12:42:44 -0600

    initial commit

---
+++ drafts/index02.php
@@ -0,0 +1,20 @@
+<?php $thisPage="index02"; ?>
+<?php include 'header.php'; ?> 
+<style>      
+body {
+	background-color: #333;
+	/* background-image: url(_images/xyz.jpg);
+	background-repeat: repeat-y; */
+}
+</style>
+</head>
+<body>
+
+<div class="page2">
+	<img src="_images/index-02.jpg">
+</div>       
+ 
+ 
+<?php include 'navigation.php'; ?>  
+</body>
+</html>
---
+++ drafts/log.php
@@ -0,0 +1,32 @@
+<?php $thisPage="log"; ?>
+<?php include 'header.php'; ?> 
+</head>
+<body>
+
+<div class="log">
+	<p class="bkg_white"><strong>01</strong> - Original Draft</p>
+
+    <p class="bkg_gray"><strong>02</strong> - New direction. Taking into account the likes &amp; dislikes of the first draft I have scrapped the 1 page design in lieu of a mixed approach where a few of the items will appear on Page 1 and the rest will appear on their own individual pages.<br />
+    <br />
+	&quot;Services&quot; in the top navigation will dropdown to reveal links to all servicves.<br />
+	<br />
+	Body background is dark gray just to drive focus to content. Please shrink your browser width as the built-out site will fill the entire viewport.</p>
+
+    <p class="bkg_white"><strong>03</strong> - Mimicing prototype (top section) sent by Amy.</p>
+
+    <p class="bkg_gray"><strong>04</strong> - Dialed everything way back. 2 fonts, 3 sizes and 3 complimentary earth tone/business colors. Footer designated as &quot;at-a-glance&quot; sitemap. Also made logo completely black &amp; removed the 1px stroke.</p>
+
+    <p class="bkg_white"><strong>05</strong> - Single page design.</p>
+
+
+    <!--
+    <p class="bkg_gray"><strong>06</strong> - 06 notes go here...</p>
+    <p class="bkg_white"><strong>07</strong> - 07 notes go here...</p>
+    <p class="bkg_gray"><strong>08</strong> - 08 notes go here...</p>
+-->
+</div>
+
+<?php include 'navigation.php'; ?>  
+
+</body>
+</html>
