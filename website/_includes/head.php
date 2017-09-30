3c61f001a5ffb2cd84ee3fcb03d19fdd0f03d478
robertmeans <robert@robertmeans.com>
2017-09-30 10:28:15 -0600

    a start

---
+++ website/_includes/head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+  Author:       Robert Means
+                Evergreen Web Design
+  Contact:      robert@evergreenwebdesign.com
+                (303) WEBSITE
+                that's (303) 932.7483
+  Last Update:  September 30, 2017
+  Comments:     You look very nice today! :)
+-->
+<head>
+  <meta charset="UTF-8">
+  <title>Circle of Animals, Dr. Amy Norton - Denver Colorado Front Range Mobile Vetinary Services</title>
+  <link rel="icon" type="image/ico" href="_images/favicon.ico">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="description" content="Dr. Amy Norton provides a full spectrum of mobile veterinary services for west Denver and the Front Range. Call (303) 653-8388">
+  <meta name="format-detection" content="telephone=no">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400|PT+Serif" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="_scripts/modernizr.js?<?php echo time(); ?>"></script>
+  <link rel="stylesheet" href="style.css?<?php echo time(); ?>">
+</head>
--- website/index.php
+++ website/index.php
@@ -1,28 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<!--
-  Author:       Robert Means
-                Evergreen Web Design
-  Contact:      robert@evergreenwebdesign.com
-                (303) WEBSITE
-                that's (303) 932.7483
-  Last Update:  September 29, 2017
-  Comments:     You look very nice today! :)
--->
-<head>
-  <meta charset="UTF-8">
-  <title>Circle of Animals, Dr. Amy Norton Denver Colorado Front Range Mobile Vetinary Services</title>
-  <link rel="icon" type="image/ico" href="_images/favicon.ico">
-  <link rel="image_src" href="https://www.evergreenwebdesign.com/_images/thumbnails/EWD-thumb-01.png" />
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta name="description" content="Evergreen Web Design is a full-service Internet marketing firm offering custom solutions to virtually any marketing project. Call (303) WEBSITE">
-  <meta name="format-detection" content="telephone=no">
-  <meta name="theme-color" content="#134b86">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script src="_scripts/modernizr.js?<?php echo time(); ?>"></script>
-  <link rel="stylesheet" href="style.css?<?php echo time(); ?>">
-<body>
+<?php require('_includes/head.php'); ?>
 
+<body>
+<div id="top-of-page"></div>
+<a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+<header id="hero">
+  <section id="slogan">
+    <h1>Circle of Animals</h1>
+    <h2>Providing Options and Guidance</h2>
+  </section>
+</header>
 
 
 <script src="js/scripts.js?<?php echo time(); ?>"></script>
