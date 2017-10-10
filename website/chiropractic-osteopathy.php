c5a757853d60bfdefa44d5313886bfe7284df388
robertmeans <robert@robertmeans.com>
2017-10-09 18:36:18 -0600

    commit before merge

---
+++ website/chiropractic-osteopathy.php
@@ -0,0 +1,27 @@
+<?php require('_includes/head.php'); ?>
+
+<body>
+<!-- <div id="top-of-page"></div> -->
+<a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- <img class="logo sm-gone" src="_images/coanimals-logo.png"> -->
+<header id="hero-alt">
+<?php require('_includes/nav.php'); ?>
+
+</header>
+
+<section id="chiropractic-osteopathy">
+  
+
+</section>
+
+
+
+
+<?php require('_includes/footer.php'); ?>
+
+<script src="js/nav.js?<?php echo time(); ?>"></script>
+<script src="js/scripts.js?<?php echo time(); ?>"></script>
+<script src="http://localhost:35729/livereload.js"></script>	
+</body>
+</html>
